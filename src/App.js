c71ff4433cb9ddcacbd8560e7939d9ed7b0c5638
Kevin <kevin07liu@gmail.com>
2018-04-19 13:30:27 +0600

    feat: add Spinner component for handling logining idle state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import firebaseConfig from '../firebase_config';
+import { Config } from '../firebase_config';
 
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
-    firebase.initializeApp(firebaseConfig)
+    firebase.initializeApp(Config);
   }
   render() {
     return (
